8dfeeeff0384da7185a3bd8c4d402aeddc59a5df
Breno Nunes <Brenontavares@gmail.com>
2019-01-24 14:55:28 -0200

    organizing style of news

--- js/carrossel.js
+++ js/carrossel.js
@@ -2,12 +2,13 @@ $(document).ready(function () {
     $('.single-item').slick({
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 750,
         slidesToShow: 1,
         slidesToScroll: 1,
         // adaptiveHeight: true,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        adaptiveHeight: true,
+        appendArrows: '',
     });
 });
