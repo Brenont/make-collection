1c5c3988afb0799059e72dd2079b72f34a865b0f
Breno Nunes <Brenontavares@gmail.com>
2019-01-24 01:07:17 -0200

    slick carrossel

---
+++ js/carrossel.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.single-item').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // adaptiveHeight: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        arrows: true,
+    });
+});
