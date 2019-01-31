6f4e712471c09acf06179668e51912af28c8fb4d
Breno Nunes <Brenontavares@gmail.com>
2019-01-31 11:00:19 -0200

    no scroll on loading & shadow in pics

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,11 @@
+var body = document.querySelector("body");
+body.style.overflow = "hidden"
+
 window.addEventListener("load", function () {
     var loader = document.querySelector("#loader");
     loader.classList.add("hide-loader");
     setTimeout(function () {
         loader.style.display = "none";
+        body.style.overflow = "initial";
     }, 1500);
 })
